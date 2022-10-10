e05861275f6e2aede42b3dbe5778d3bc68475b9a
BOYEONJEONG <qhdus96@naver.com>
2022-10-10 13:22:58 +0900

    commit GSAP(badge)
    - lodash _.throttle 스크롤 함수 과부하 방지
    - gsap.to 애니메이션

--- CloneCoding/Starbucks/js/main.js
+++ CloneCoding/Starbucks/js/main.js
@@ -14,3 +14,32 @@ searchInputEl.addEventListener("blur", () => {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+// _.throttle(함수, 시간)
+// lodsh cdn
+// https://cdnjs.com/libraries/lodash.js
+// https://lodash.com/
+
+// gsap cdn
+// https://cdnjs.com/libraries/gsap  >> index.html에 붙여넣기
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // 배치 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배치 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 500)
+);
